4d1817aad98c8866abf5ad2458a0d132e10a385f
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-07-09 01:10:44 -0300

    switch

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,48 @@ import Exercises.Palindrome;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+   static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-       /* List<String> fizzbuzz = new ArrayList<>();
-        fizzbuzz = FizzBuzz.fizzBuzz(15);
-        fizzbuzz.forEach(ob -> System.out.println(ob));
-      */
-        boolean palindrome = Palindrome.isPalindrome(4774);
-        System.out.println(palindrome);
+        boolean op = false;
+        do{
+            Menu();
+            int opc = scan.nextInt();
+            switch (opc){
+                case 1:
+                    List<String> fizzbuzz;
+                    fizzbuzz = FizzBuzz.fizzBuzz(15);
+                    fizzbuzz.forEach(ob -> System.out.println(ob));
+                    break;
+                case 2:
+                    boolean palindrome = Palindrome.isPalindrome(4774);
+                    System.out.println(palindrome);
+                    palindrome = Palindrome.isPalindrome(543);
+                    System.out.println(palindrome);
+                    break;
+            }
+
+        }while (op == true);
+        
+
+    }
+
+    public static void Menu(){
+        System.out.println(" '1' - FizzBuzz");
+        System.out.println(" '2' - Palindrome");
+        System.out.println(" '2' - Palindrome");
+
+        System.out.println(" '2' - Palindrome");
+
+        System.out.println(" '2' - Palindrome");
+        System.out.println(" '2' - Palindrome");
+
+        System.out.println(" '2' - Palindrome");
 
     }
 }
